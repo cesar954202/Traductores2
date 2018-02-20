2d8590094bc9e42b41be27e8a39ab27f3b6597ee
Unknown <cesar95.4202@gmail.com>
2018-02-20 14:06:53 -0600

    Implementacion de estados listas en test

--- Proyecto.py
+++ Proyecto.py
@@ -1,8 +1,12 @@
+class token:
+    def __init__(self,dato,tipo):
+        self.dato = dato
+        self.tipo = tipo
+        
+    def mostrar(self):
+        print "Dato: "  , self.dato  , " Tipo: " , self.tipo
 
 
-
-
-print "Bienvenido a mi traductor".center(80,"=")
 
 estados = [[1,4,40,6,40,40,17,11,40,40,40,40,40,40,40,40,0,40,23,24,25,33,31,29,27,36,37,34,39,35,38],
 [1,4,40,6,40,40,17,11,40,40,40,40,40,40,40,40,0,40,23,24,25,33,31,29,27,36,37,34,2,39,38],
@@ -51,6 +55,10 @@ estado = 0
 nuevoEstado = 0
 entrada = 0
 salida = 1
+tokens = []
+datToken = ""
+
+print "Bienvenido a mi traductor".center(80,"=")
 
 while salida !=0:
     
@@ -64,103 +72,105 @@ while salida !=0:
     while indice < len(contenido):
         print contenido[indice]
         if contenido[indice].isdigit():
-            print "Es numero"
+            #print "Es numero"
             entrada = 0
         elif 'a' <= contenido[indice] <= 'z':
             print "Es un caracter de la a-z"
             #Validar no sea una palabra reservada if,while,return,else,int,float
             if(contenido[indice] == 'i'):
-                print "Es una i"
+                #print "Es una i"
                 entrada = 1
             elif(contenido[indice] == 'f'):
-                print "Es una f"
+                #print "Es una f"
                 entrada = 2
             elif(contenido[indice] == 'w'):
-                print "Es una w"
+                #print "Es una w"
                 entrada = 3
             elif(contenido[indice] == 'h'):
-                print "Es una h"
+                #print "Es una h"
                 entrada = 4
             elif(contenido[indice] == 'l'):
-                print "Es una l"
+                #print "Es una l"
                 entrada = 5
             elif(contenido[indice] == 'e'):
-                print "Es una e"
+                #print "Es una e"
                 entrada = 6
             elif(contenido[indice] == 'r'):
-                print "Es una r"
+                #print "Es una r"
                 entrada = 7
             elif(contenido[indice] == 't'):
-                print "Es una t"
+                #print "Es una t"
                 entrada = 8
             elif(contenido[indice] == 'u'):
-                print "Es una u"
+                #print "Es una u"
                 entrada = 9
             elif(contenido[indice] == 'n'):
-                print "Es una n"
+                #print "Es una n"
                 entrada = 10
             elif(contenido[indice] == 'l'):
-                print "Es una l"
+                #print "Es una l"
                 entrada = 11
             elif(contenido[indice] == 's'):
-                print "Es una s"
+                #print "Es una s"
                 entrada = 12
             elif(contenido[indice] == 'o'):
-                print "Es una o"
+                #print "Es una o"
                 entrada = 13
             elif(contenido[indice] == 'a'):
-                print "Es una a"
+                #print "Es una a"
                 entrada = 14
             else:
-                print "Cualquier contenido[indice] de a-z"
+                #print "Cualquier contenido[indice] de a-z"
                 entrada = 15
         elif contenido[indice].isspace():
-            print "Es un espacio"
+            #print "Es un espacio"
             entrada = 16
         elif 'A' <= contenido[indice] <= 'Z':
-            print "Es una mayuscula"
+            #print "Es una mayuscula"
             entrada = 17
         elif (contenido[indice] == '+' or contenido[indice] == '-'):
-            print "Es un operador de adicion"
+            #print "Es un operador de adicion"
             entrada = 18
         elif (contenido[indice] == '*' or contenido[indice] == '/'):
-            print "Es operador de multiplicacion"
+            #print "Es operador de multiplicacion"
             entrada = 19
         elif (contenido[indice] == '<' or contenido[indice] == '>'):
-            print "Es un operador relacional"
+            #print "Es un operador relacional"
             entrada = 20
         elif (contenido[indice] == '='):
-            print "asignacion"
+            #print "asignacion"
             entrada = 21
         elif (contenido[indice] == '!'):
-            print "Operador relacional opNot"
+            #print "Operador relacional opNot"
             entrada = 22
         elif (contenido[indice] == '&'):
-            print "Es un operador and"
+            #print "Es un operador and"
             entrada = 23
         elif (contenido[indice] == '|'):
-            print "Es un operador OR"
+            #print "Es un operador OR"
             entrada = 24
         elif (contenido[indice] == '(' or contenido[indice] == ')'):
-            print "Parentesis"
+            #print "Parentesis"
             entrada = 25
         elif (contenido[indice] == '{' or contenido[indice] == '}'):
             print "Llaves"
-            entrada = 26
+            #entrada = 26
         elif (contenido[indice] == ';'):
             print "Punto y coma"
-            entrada = 27
+            #entrada = 27
         elif (contenido[indice] == '.'):
             print "Es un punto"
-            entrada = 28
+            #entrada = 28
         elif(contenido[indice] == ','):
 	    print "Coma"
-	    entrada = 29
+	    #entrada = 29
 	else:
-            print "contenido[indice] Invalido"
+            #print "CARACTER Invalido"
             entrada = 30
             
-        print "Entrada : ", entrada
+        #print "Entrada : ", entrada
+
+        datToken = datToken + contenido[indice]
         
         nuevoEstado = estados[estado][entrada]
         print "Nuevo Estado : ", nuevoEstado
@@ -169,14 +179,29 @@ while salida !=0:
         #if(estado == 1):
         #   if(entrada != 0 and entrada != 28)
         #       agregar tipo de token y valor
+
+        if (estado == 0) and (entrada == 0):
+                              dataToken = ""
+        elif (estado == 1) and (0 < entrada < 27 or entrada > 28):
+                              aux = token(datToken, 9)
+                              tokens.append(aux)
+                              dataToken = ""
+        elif (estado == 2) and (entrada < 0):
+            aux = token(datToken, 7)
+            tokens.append(aux)
+            dataToken = ""
+        elif (estado == 3) and (entrada < 0):
+            aux = token(datToken, 3)
+            tokens.append(aux)
+            dataToken = ""
+        elif (estado == 4) and (entrada < 16 or ):
         
         
+        #####Terminana if else para guardar tokens
 
         estado  = nuevoEstado
         indice += 1
 
-        
-      
 
     salida = int(input("Desea salir presione 0: "))
     estado = 0
--- Test.py
+++ Test.py
@@ -1,132 +1,59 @@
-print "Bienvenido a mi traductor".center(80,"=")
 
-test = [[0,1,2],
-           [10,11,13],
-           [20,21,22]
-           ]
+class token:
+    def __init__(self,dato,tipo):
+        self.dato = dato
+        self.tipo = tipo
+        
+    def mostrar(self):
+        print "Dato: "  , self.dato  , " Tipo: " , self.tipo
+        
 
-estado = 0
-entrada = 0
+aux = token("1.1" , 0)
 
-salida = 1
-while salida !=0:
+aux.dato = aux.dato + "2"
+
+aux.dato = float(aux.dato)
+
+aux.dato = aux.dato + 1
+
+tokens = []
+
+tokens.append(aux)
+
+for pos in tokens:
+    pos.mostrar()
+    #aux.dato += 1
+    #tokens.append(aux)
     
-    #Se abre el archivo y se guarda en variable
-    entrada = raw_input("Escribe el nombre de archivo: ")
-    archivo = open(entrada, "r")
-    contenido = archivo.read()
-    print contenido
+entrada = 29
+estado = 2
+if (estado == 2) and (0 < entrada < 27 or entrada > 28):
+    print "Si se valido"
 
-    #Pruebas de impresion de posision
-#    pos = int(input("Imprimir pos: "))
-#    pos2 = int(input("Imprimir pos2: "))
+
+#test = [[0,1,2],
+#           [10,11,13],
+#           [20,21,22]
+#           ]
+
+#estado = 0
+#entrada = 0
+
+#salida = 1
+#while salida !=0:
     
-#    aux = estados[pos][pos2]
-#    print estado
+#Se abre el archivo y se guarda en variable
+#entrada = raw_input("Escribe el nombre de archivo: ")
+#archivo = open(entrada, "r")
+#contenido = archivo.read()
+#print contenido
 
-    #for contenido[indice] in contenido:
-    indice = 0
-    while indice < len(contenido):
-        print contenido[indice]
-        if contenido[indice].isdigit():
-            print "Es numero"
-            entrada = 0
-        elif 'a' <= contenido[indice] <= 'z':
-            print "Es un caracter de la a-z"
-            #Validar no sea una palabra reservada if,while,return,else,int,float
-            if(contenido[indice] == 'i'):
-                print "Es una i"
-                entrada = 1
-            elif(contenido[indice] == 'f'):
-                print "Es una f"
-                entrada = 2
-            elif(contenido[indice] == 'w'):
-                print "Es una w"
-                entrada = 3
-            elif(contenido[indice] == 'h'):
-                print "Es una h"
-                entrada = 4
-            elif(contenido[indice] == 'l'):
-                print "Es una l"
-                entrada = 5
-            elif(contenido[indice] == 'e'):
-                print "Es una e"
-                entrada = 6
-            elif(contenido[indice] == 'r'):
-                print "Es una r"
-                entrada = 7
-            elif(contenido[indice] == 't'):
-                print "Es una t"
-                entrada = 8
-            elif(contenido[indice] == 'u'):
-                print "Es una u"
-                entrada = 9
-            elif(contenido[indice] == 'n'):
-                print "Es una n"
-                entrada = 10
-            elif(contenido[indice] == 'l'):
-                print "Es una l"
-                entrada = 11
-            elif(contenido[indice] == 's'):
-                print "Es una s"
-                entrada = 12
-            elif(contenido[indice] == 'o'):
-                print "Es una o"
-                entrada = 13
-            elif(contenido[indice] == 'a'):
-                print "Es una a"
-                entrada = 14
-            else:
-                print "Cualquier contenido[indice] de a-z"
-                entrada = 15
-        elif contenido[indice].isspace():
-            print "Es un espacio"
-            entrada = 16
-        elif 'A' <= contenido[indice] <= 'Z':
-            print "Es una mayuscula"
-            entrada = 17
-        elif (contenido[indice] == '+' or contenido[indice] == '-'):
-            print "Es un operador de adicion"
-            entrada = 18
-        elif (contenido[indice] == '*' or contenido[indice] == '/'):
-            print "Es operador de multiplicacion"
-            entrada = 19
-        elif (contenido[indice] == '<' or contenido[indice] == '>'):
-            print "Es un operador relacional"
-            entrada = 20
-        elif (contenido[indice] == '='):
-            print "asignacion"
-            entrada = 21
-        elif (contenido[indice] == '!'):
-            print "Operador relacional opNot"
-            entrada = 22
-        elif (contenido[indice] == '&'):
-            print "Es un operador and"
-            entrada = 23
-        elif (contenido[indice] == '|'):
-            print "Es un operador OR"
-            entrada = 24
-        elif (contenido[indice] == '(' or contenido[indice] == ')'):
-            print "Parentesis"
-            entrada = 25
-        elif (contenido[indice] == '{' or contenido[indice] == '}'):
-            print "Llaves"
-            entrada = 26
-        elif (contenido[indice] == ';'):
-            print "Punto y coma"
-            entrada = 27
-        elif (contenido[indice] == '.'):
-            print "Es un punto"
-            entrada = 28
-        elif(contenido[indice] == ','):
-	    print "Coma"
-	    entrada = 29
-	else:
-            print "contenido[indice] Invalido"
-            entrada = 30
-            
-        print "Entrada : ", entrada
-        print " "
-        indice += 1
-        print "Indice es " , indice , "largo" , len(contenido)
-    salida = int(input("Desea salir presione 0: "))
+#Pruebas de impresion de posision
+#pos = int(input("Imprimir pos: "))
+#pos2 = int(input("Imprimir pos2: "))
+
+#imprimir variables
+#print "Entrada : ", entrada
+#print "Indice es " , indice , "largo" , len(contenido)
+
+
